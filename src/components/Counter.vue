24f853a755bf8d09e4c7737ca1def2ca56428d4b
Annalartey <74235952+Annalartey@users.noreply.github.com>
2023-03-18 23:07:12 +0000

    Update Counter.vue

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,15 +2,15 @@
     <div class="main">
        <h1>Welcome,</h1>
    
-       <div>
+       <div class="guess">
         <div>
           <div>{{ message }}</div>
           <p>Tell us your answer. Lets see if you guessed right.</p>
           <input type="number" v-model="$store.state.guess"/>
           <button @click = "$store.commit ( 'handleGuess' ) " :disabled="!$store.state.guess" >Enter</button>
         </div>
-        <div>
-          <p>Guessed Number is:</p>
+        <div class="guess-no">
+          <h3>Your guessed Number is: </h3>
           <p>{{ $store.state.guessed }}</p>
         </div>
        </div>
@@ -25,7 +25,7 @@
       <button @click = "$store.dispatch ( 'handleDecrease' ) " :disabled="$store.state.count < 1" class="operation"> - </button>
       <button @click = "$store.commit ( 'handleReset' ) " :disabled="$store.state.count < 1" class="operation">Reset</button>
      </div>
-      <div>
+      <div class="setval">
         <input type="number" v-model="$store.state.newCount"/>
         <button @click="$store.commit ( 'handleSetValue' ) ">Set value</button>
       </div>
@@ -70,15 +70,33 @@
     a {
       color: #42b983;
     }
-    .main{
-      
+    .guess{
+      display: flex;
+      align-items: space-between;
+      justify-content: center;
     }
-    button{
+    .guess-no{
+      border:#42b983 solid 2px;
+      padding: 8px;
+      margin: 0 8px;
+      font-size: larger;
+    }
+    .guess-no .p{
+      font-size: xx-large;
+      color: #42b983;
+    }
+    .setval{
+      display: flex;
+      align-items: space-between;
+      justify-content: center;
+    }
+  button{
   color: #42b983;
   border: solid 2px #42b983 ;
   border-radius: 8px;
   padding: 4px 12px;
   font-size: large;
+  margin: 0 4px;
  }
  button:hover{
   color: #ffffff;
@@ -87,7 +105,7 @@
  }
  input{
   width: 100%;
-  padding: 12px 20px;
+  padding: 4px 20px;
   margin: 8px 0;
   display: inline-block;
   border: 1px solid #ccc;
