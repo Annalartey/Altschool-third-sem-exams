c161acedaf23340bf3b44778c6485feb1c090287
Annalartey <74235952+Annalartey@users.noreply.github.com>
2023-03-18 22:40:29 +0000

    Update Counter.vue

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -85,4 +85,13 @@
   background-color: #42b983;
   cursor: pointer;
  }
+ input{
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+ }
     </style>
